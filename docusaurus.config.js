aca1293d8c8b179fb8625edb4c833ac5aa3120bc
Soumadip Das <soumadip@zohomail.in>
2025-06-02 01:19:12 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,8 @@ const config = {
   organizationName: usrConf.about_me.title,
   deploymentBranch: "site",
 
-  onBrokenLinks: "ignore",
+  onBrokenAnchors: "ignore",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   i18n: {
