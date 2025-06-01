5a66548d6471a0368a2073bdce685b2ef31d3a0e
soymadip <soumadip@zohomail.in>
2025-06-01 12:41:57 +0530

    optimized workflow

--- .github/compile.sh
+++ .github/compile.sh
@@ -1,5 +1,6 @@
 
+echo ">>> Compiling because upstream has new update"
 echo "This is a test script to check the compilation of the code."
 sleep 2
 ls
-echo "Script run Successfully"
+echo ">>> Script ran Successfully"
